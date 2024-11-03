aaf5265c97189d601b25118b62da415ed5cbeeea
juules26 <juules26@gmail.com>
2024-11-03 15:09:04 +0100

    backend ready to deploy in render

---
+++ mongo.js
@@ -0,0 +1,45 @@
+// 1. Import mongoose library
+const mongoose = require('mongoose')
+
+// 2. Verify if there is password as argument
+if (process.argv.length < 3) {
+    console.log(`provide a password as argument`)
+    process.exit(1) // exit program with error message
+}
+
+// 3. Define URl and password
+const password = process.argv[2]
+const url = `mongodb+srv://juules26:${password}@cluster0.cvvzf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+// 4. Connect to mongodb
+mongoose.connect(url)
+
+// 5. Define Mongoose scheme
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+// Define scheme based model
+const Person = mongoose.model('Person', personSchema)
+
+// 6. Add or list people
+if (process.argv.length === 5) {
+    const person = new Person ({
+        name: process.argv[3],
+        number: process.argv[4]
+    })
+
+    person.save().then(() => {
+        console.log(`added ${person.name} number ${person.number} to phonebook`)
+        mongoose.connection.close()
+    })
+} else if (process.argv.length === 3) {
+    // List all the people
+    Person.find({}).then(result => {
+        console.log('phonebook:')
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        });
+        mongoose.connection.close() // 7. Close connection
+    })
+}
--- server.js
+++ server.js
@@ -1,31 +1,33 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
 const path = require('path')
+const Person = require('./models/phonebook') // importa el modelo
 
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-];
+// let persons = [
+//   { 
+//     "id": 1,
+//     "name": "Arto Hellas", 
+//     "number": "040-123456"
+//   },
+//   { 
+//     "id": 2,
+//     "name": "Ada Lovelace", 
+//     "number": "39-44-5323523"
+//   },
+//   { 
+//     "id": 3,
+//     "name": "Dan Abramov", 
+//     "number": "12-43-234345"
+//   },
+//   { 
+//     "id": 4,
+//     "name": "Mary Poppendieck", 
+//     "number": "39-23-6423122"
+//   }
+// ];
 
 app.use(cors())
 app.use(express.json())
@@ -34,82 +36,137 @@ app.use(express.static(path.join(__dirname, 'dist')))
 
 morgan.token('req-body', (request) => JSON.stringify(request.body))
 
+// Home route
 app.get('/', (request, response) => {
   response.send('Welcome to the Phonebook :-)')
 })
 
+// Fetch all persons from MongoDB
 app.get('/api/persons', (request, response) => {
-  response.json(persons);
-});
+  Person.find({}) // Busca todos los documentos en la coleccion
+    .then(persons => {
+      response.json(persons)
+    })
+    .catch(error => {
+      console.log(error.name)
+      next(error)
+    })
+})
 
+// Additional info 
 app.get('/api/info', (request, response) => {
-  const listLength = persons.length
-  const date = new Date().toString()
-  const message = `Phonebook has info for ${listLength} people <br/><br/>${date}`
-
-  response.send(message)
+  Person.countDocuments({})
+    .then(count => {
+      const date = new Date().toString()
+      const message = `Phonebook has info for ${count} people <br/><br/>${date}`
+      response.send(message)
+    })
+    .catch(error => {
+      console.log(error.name)
+      next(error)
+    })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-    console.error('id non existing')
-  }
+// Fetch person by ID from Mongo
+app.get('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
+
+  Person.findById(id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).send({ error: 'Person not found' })
+      }
+    })
+    .catch(error => {
+      console.log(error.name)
+      next(error)})
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const personToDelete = persons.find(person => person.id === id)
-  if (personToDelete) {
-    console.log(`${personToDelete.name} deleted`)
-  }
-  persons = persons.filter(person => person.id !== id)
-  response.status(204).end()
+// Update contact number
+app.put('/api/persons/:id', (request, response, next) => {
+  const { number } = request.body //extract number from request body
+  const id = request.params.id // get id from url
+
+  Person.findByIdAndUpdate( id, { number }, { new: true } ) // searches database for person with specific id
+    .then(updatedPerson => { // after finding and updating, .then() gets called with results of update
+      if (updatedPerson) { // if person found and updated
+        response.json(updatedPerson) // sends updated person to client (frontend) as a JSON response
+      } else {
+        response.status(404).send({ error: 'Person not found' })
+      }
+    })
+    .catch(error => next(error))
+}) 
+
+// Delete person by ID from Mongo
+app.delete('/api/persons/:id', (request, response, next) => {
+  const id = request.params.id
+
+  Person.findById(id)
+    .then(person => {
+      if (person) {
+        console.log('Deleted person:', person)
+        return person.deleteOne()
+      } else {
+        response.status(404).send({ error: 'Person not found' })
+      }
+    })
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => {
+      console.log(error.name)
+      next(error)})
 })
 
-const generateId = () => {
-  const idLimit = 10000
-  const randomId = Math.floor(Math.random() * idLimit) + 4
-  return randomId
-}
+// Add new person to Mongo
+app.post('/api/persons', (request, response, next) => {
+  const {name, number} = request.body
 
-app.post('/api/persons',(request, response) => {
-  const body = request.body
-
-  console.log('request POST:', request.body)
-  if (!body.name || !body.number) {
+  if (!name || !number) {
     return response.status(400).json({
       error: 'name or number missing and required'
     })
   }
-  const nameExists = persons.some(person => person.name === body.name)
-  if (nameExists) {
-    console.log(`${body.name} exists!`)
-    return response.status(409).json({
-      error: `${body.name} already exists`
+  const person = new Person({ name, number })
+
+  person.save()
+    .then(savedPerson => {
+      console.log('Added person:', savedPerson)
+      response.status(201).json(savedPerson)
     })
-  }
-
-  const person = {
-    id: generateId(),
-    name: body.name,
-    number: body.number
-  }
-
-  persons.push(person)
-  console.log(`${person.name} added to contacts`)
-  response.status(201).json(person)
+    .catch(error => {
+      console.log(error.name)
+      next(error)})
 })
 
+// Fallback route for non-existent paths (for SPA)
 app.get('*', (request, response) => {
   response.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error('Error:', error.name, error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+  response.status(500).send({ error: 'Internal server error' })
+}
+
+app.use(errorHandler)
